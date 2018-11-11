5090670ebc8d59616eede33e84d5b25fa7093a57
Irish de Jesus <Irish.deJesus@durbi.com.ph>
2018-11-11 19:40:04 +0800

    Routing via annotations and Controller

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+	/**
+	 * @Route("/")
+	 */
+	public function homepage(){
+		return new Response('OMG! My Frist Page!!');
+	}
+
+	/**
+	 * @Route("/task/{slug}")
+	*/
+	public function show($slug){
+		
+		return new Response(sprintf(
+				'this is my route: %s', $slug
+			));
+	}
+
+}
+
+?>
