1312015a7995979be42ab7fb81692dfda7003025
Irish de Jesus <Irish.deJesus@durbi.com.ph>
2018-11-11 20:52:52 +0800

    Twig Templating

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 	/**
 	 * @Route("/")
@@ -19,9 +20,20 @@ class ArticleController
 	*/
 	public function show($slug){
 		
-		return new Response(sprintf(
-				'this is my route: %s', $slug
-			));
+		$comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+		return  $this->render('article/show.html.twig', [
+					'urltitle' => ucwords(str_replace('-', ' ', $slug)),
+					'comments' => $comments
+				]);
+
+		// return new Response(sprintf(
+		// 		'this is my route: %s', $slug
+		// 	)); 
 	}
 
 }
