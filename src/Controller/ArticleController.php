a26b0d1a7287740b73fd6d1d56c677c1a1d830e3
Irish de Jesus <Irish.deJesus@durbi.com.ph>
2018-11-25 13:07:38 +0800

    sunday tuts, services and bundles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class ArticleController extends AbstractController
 {
@@ -19,7 +22,7 @@ class ArticleController extends AbstractController
 	/**
 	 * @Route("/task/{slug}", name="show_article")
 	*/
-	public function show($slug){
+	public function show($slug, AdapterInterface $cache){
 		
 		$comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -27,9 +30,20 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
+        $articleContent = 'this aaa is a test';
+
+        $cacheData = $cache->getItem('mardown'.md5($articleContent));
+
+        if(!$cacheData->isHit()){
+        	$cacheData->set($articleContent);
+        	$cache->save($cacheData);
+        }
+        $articleContent = $cacheData->get();
+
 		return  $this->render('article/show.html.twig', [
 					'urltitle' => ucwords(str_replace('-', ' ', $slug)),
 					'comments' => $comments,
+					'articleContent' => $articleContent,
 					'slug' => $slug
 				]);
 
@@ -41,8 +55,9 @@ class ArticleController extends AbstractController
 	/**
 	 * @Route("/task/{slug}/heart", name="article_toogle", methods = {"POST"})
 	*/	
-	public function toogleArticleLike($slug){
+	public function toogleArticleLike($slug, LoggerInterface $logger){
 		// return new Response(json_encode(['heart' => 5]));
+		$logger->info('Article is being hearted');
 		return new JsonResponse(['heart' => 1]);
 
 	}
